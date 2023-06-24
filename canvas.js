12a431b5efb9f12f3e09fc3b6400e53383a5798e
Marina Shemesh <marina.shemesh1@gmail.com>
2023-06-24 11:57:16 +0300

    Add files via upload

---
+++ canvas.js
@@ -0,0 +1,12 @@
+console.log('hello from the console');
+
+const canvas = document.querySelector ('canvas')
+//console.log(canvas);
+const context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+context.fillRect(100, 100, 100, 100);
+
